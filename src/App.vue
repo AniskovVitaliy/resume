d6ee0316f07d6adde184e35e4dbc3735806e5ca9
Vitaliy <organizm78by@gmail.com>
2024-11-05 20:11:25 +0300

    added buttons for uploading cv

--- src/App.vue
+++ src/App.vue
@@ -34,13 +34,34 @@ function setLanguage(value) {
     language.value = value
 }
 
+function downloadFile(url, fileName) {
+    const a = document.createElement('a');
+    a.href = url;
+    a.download = fileName;
+    a.style.display = 'none';
+    document.body.appendChild(a);
+    a.click();
+    document.body.removeChild(a);
+}
 </script>
 
 <template>
-    <div class="fixed top-2 right-2 bg-zinc-700 text-white">
-        <button class="m-4" @click="setLanguage('ru')">RU</button>
-        <button class="m-4" @click="setLanguage('en')">EN</button>
-<!--        <button class="m-4" @click="exportToPdf">CV</button>-->
+    <div class="fixed flex top-2 right-2 bg-zinc-700 text-white">
+        <button class="px-4 py-2" :class="{'font-bold':language === 'ru'}" @click="setLanguage('ru')">RU</button>
+        <button class="px-4 py-2" :class="{'font-bold':language === 'en'}" @click="setLanguage('en')">EN</button>
+        <button class="flex px-4 py-2" @click="downloadFile('./aniskov-vitaliy_ru.pdf', 'aniskov-vitaliy-resume-ru.pdf')">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+                <path stroke-linecap="round" stroke-linejoin="round" d="M3 16.5v2.25A2.25 2.25 0 0 0 5.25 21h13.5A2.25 2.25 0 0 0 21 18.75V16.5M16.5 12 12 16.5m0 0L7.5 12m4.5 4.5V3" />
+            </svg>
+            CV [RU]
+        </button>
+        <button class="flex px-4 py-2" @click="downloadFile('./aniskov-vitaliy_en.pdf', 'aniskov-vitaliy-resume-en.pdf')">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+                <path stroke-linecap="round" stroke-linejoin="round" d="M3 16.5v2.25A2.25 2.25 0 0 0 5.25 21h13.5A2.25 2.25 0 0 0 21 18.75V16.5M16.5 12 12 16.5m0 0L7.5 12m4.5 4.5V3" />
+            </svg>
+            CV [EN]
+        </button>
+
     </div>
     <div class="flex font-sans max-lg:flex-col-reverse" id="element-to-convert">
         <div class="flex justify-center p-10 bg-zinc-700">
